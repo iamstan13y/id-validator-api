447bd5108621f73543fdfa4043fd92db11c52aae
iamstan13y <iamstan13y@gmail.com>
2023-07-22 00:58:04 +0200

    Added Validation Controller.

---
+++ IDValidator.API/Controllers/ValidationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDValidator.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ValidationController : ControllerBase
+    {
+    }
+}
