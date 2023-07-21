159abd02365ff2b0f8a305982789f655516c272b
iamstan13y <iamstan13y@gmail.com>
2023-07-21 23:27:34 +0200

    Configured Validator Service & DI.

---
+++ IDValidator.API/Services/IValidatorService.cs
@@ -0,0 +1,8 @@
+﻿using IDValidator.API.Models.Local;
+
+namespace IDValidator.API.Services;
+
+public interface IValidatorService
+{
+    Task<Result<bool>> CheckAuthenticityAsync(string idNumber);
+}
